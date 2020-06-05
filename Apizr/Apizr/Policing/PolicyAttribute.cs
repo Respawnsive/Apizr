438b4b9eb636de89a462e960d76bd270de85465a
Jérémy BRUN-PICARD <jeremy.brunpicard@respawnsive.com>
2020-06-05 11:40:09 +0200

    Including some policy files

---
+++ Apizr/Apizr/Policing/PolicyAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Apizr.Policing
+{
+
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Interface | AttributeTargets.Method)]
+    public class PolicyAttribute : Attribute
+    {
+        /// <summary>
+        /// You must add your registry to IServiceCollection to use this attribute <example>services.AddPolicyRegistry(YourRegistry);</example>
+        /// </summary>
+        /// <param name="registryKeys">Policy registry keys</param>
+        public PolicyAttribute(params string[] registryKeys)
+        {
+            this.RegistryKeys = registryKeys;
+        }
+
+        public string[] RegistryKeys { get; }
+    }
+}

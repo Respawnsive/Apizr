b1e8afd6551f0fe4654652b91486ff2e15bc971b
Jérémy BRUN-PICARD <jeremy.brunpicard@respawnsive.com>
2021-02-03 14:51:35 +0100

    Adding a Priority attribute

---
+++ Apizr/Apizr.Integrations.Fusillade/PriorityAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Refit;
+
+namespace Apizr.Integrations.Fusillade
+{
+    public class PriorityAttribute : PropertyAttribute
+    {
+        public PriorityAttribute() : base("Priority")
+        {
+            
+        }
+    }
+}

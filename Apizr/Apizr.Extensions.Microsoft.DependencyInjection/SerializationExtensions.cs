53f68ac8e080b86a1c621be8983fbee22fcbe051
Jérémy BRUN-PICARD <jeremy.brunpicard@respawnsive.com>
2021-09-01 15:42:17 +0200

    Wip on MS IDistributedCache implementation

---
+++ Apizr/Apizr.Extensions.Microsoft.DependencyInjection/SerializationExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Apizr
+{
+    public static class SerializationExtensions
+    {
+        public static byte[] ToByteArray(this object obj)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+            var binaryFormatter = new BinaryFormatter();
+            using (var memoryStream = new MemoryStream())
+            {
+                binaryFormatter.Serialize(memoryStream, obj);
+                return memoryStream.ToArray();
+            }
+        }
+
+        public static T FromByteArray<T>(this byte[] byteArray)
+        {
+            if (byteArray == null)
+            {
+                return default;
+            }
+            var binaryFormatter = new BinaryFormatter();
+            using (var memoryStream = new MemoryStream(byteArray))
+            {
+                return (T)binaryFormatter.Deserialize(memoryStream);
+            }
+        }
+
+    }
+}

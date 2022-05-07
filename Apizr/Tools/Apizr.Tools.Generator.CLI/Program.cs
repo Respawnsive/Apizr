1f9db41d1e1a29082890148461918ad10ae77073
Jérémy BRUN-PICARD <jeremy.brunpicard@respawnsive.com>
2022-05-07 14:04:33 +0200

    [Generator] WIP on CLI

--- Apizr/Tools/Apizr.Tools.Generator.CLI/Program.cs
+++ Apizr/Tools/Apizr.Tools.Generator.CLI/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.CommandLine;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Apizr.Tools.Generator;
 using Apizr.Tools.Generator.Models;
@@ -9,50 +11,71 @@ using NSwag;
 using NSwag.CodeGeneration.CSharp;
 using NSwag.CodeGeneration.OperationNameGenerators;
 
-var swagger = "https://petstore.swagger.io/v2/swagger.json";
-var result = await OpenApiDocument.FromUrlAsync(swagger);
+var urlArg = new Argument<string>("url", "Swagger.json absolute url");
 
-var clientSettings = new ApizrGeneratorSettings
+var nsOption = new Option<string>(new[] {"--namespace", "--ns"},
+    () => Assembly.GetExecutingAssembly().GetName().Name ?? "Apizr.Tools.Generations", 
+    "Generated files namespace")
 {
-    UseActionResultType = true,
-    WrapResponses = false,
-    OperationNameGenerator = new MultipleClientsFromFirstTagAndOperationIdGenerator()
+    IsRequired = false
 };
 
-clientSettings.CodeGeneratorSettings.TemplateFactory = new ApizrTemplateFactory(clientSettings.CSharpGeneratorSettings, new Assembly[3]
+var rootCommand = new RootCommand
 {
-                typeof (CSharpGeneratorSettings).GetTypeInfo().Assembly,
-                typeof (CSharpGeneratorBaseSettings).GetTypeInfo().Assembly,
-                typeof (ApizrGeneratorSettings).GetTypeInfo().Assembly
-});
-clientSettings.CSharpGeneratorSettings.Namespace = "Cms";
-clientSettings.CSharpGeneratorSettings.ArrayType = "List";
-clientSettings.CSharpGeneratorSettings.ArrayInstanceType = "List";
-clientSettings.ResponseArrayType = "ICollection";
-clientSettings.ResponseDictionaryType = "IDictionary";
+    urlArg,
+    nsOption
+};
 
-clientSettings.ParameterArrayType = "IEnumerable";
-clientSettings.ParameterDictionaryType = "IDictionary";
-var clientGenerator = new ApizrGenerator(result, clientSettings);
-var data = clientGenerator.GetAllClientType();
-var models = clientGenerator.GetAllGenerateDtoType();
-var dir = Path.Combine("output", clientSettings.CSharpGeneratorSettings.Namespace);
-var apisPath = Path.Combine(dir, "HttpApis");
-var modelsPath = Path.Combine(dir, "HttpModels");
-
-Directory.CreateDirectory(apisPath);
-Directory.CreateDirectory(modelsPath);
-
-foreach (var api in data)
+rootCommand.SetHandler(async (string url, string ns) =>
 {
-    var file = Path.Combine(apisPath, $"{api.TypeName}.cs");
-    File.WriteAllText(file, api.Code, Encoding.UTF8);
-    Console.WriteLine($"Interface output file：{file}");
-}
-foreach (var model in models)
-{
-    var file = Path.Combine(modelsPath, $"{model.TypeName}.cs");
-    File.WriteAllText(file, model.Code, Encoding.UTF8);
-    Console.WriteLine($"Model output file：{file}");
-}
-Console.WriteLine("Hello World!");
+    var assemblies = new[]
+    {
+        typeof(CSharpGeneratorSettings).GetTypeInfo().Assembly,
+        typeof(CSharpGeneratorBaseSettings).GetTypeInfo().Assembly,
+        typeof(ApizrGeneratorSettings).GetTypeInfo().Assembly
+    };
+
+    var clientSettings = new ApizrGeneratorSettings
+    {
+        UseActionResultType = true,
+        WrapResponses = false,
+        OperationNameGenerator = new MultipleClientsFromFirstTagAndOperationIdGenerator(),
+    };
+    clientSettings.CodeGeneratorSettings.TemplateFactory = new ApizrTemplateFactory(clientSettings.CSharpGeneratorSettings, assemblies);
+    clientSettings.CSharpGeneratorSettings.Namespace = ns;
+    clientSettings.CSharpGeneratorSettings.ArrayType = "List";
+    clientSettings.CSharpGeneratorSettings.ArrayInstanceType = "List";
+    clientSettings.ResponseArrayType = "ICollection";
+    clientSettings.ResponseDictionaryType = "IDictionary";
+    clientSettings.ParameterArrayType = "IEnumerable";
+    clientSettings.ParameterDictionaryType = "IDictionary";
+
+
+    //var swagger = "https://petstore.swagger.io/v2/swagger.json";
+    var result = await OpenApiDocument.FromUrlAsync(url);
+
+    var clientGenerator = new ApizrGenerator(result, clientSettings);
+    var data = clientGenerator.GetAllClientType();
+    var models = clientGenerator.GetAllGenerateDtoType();
+    var dir = Path.Combine("output", clientSettings.CSharpGeneratorSettings.Namespace);
+    var apisPath = Path.Combine(dir, "HttpApis");
+    var modelsPath = Path.Combine(dir, "HttpModels");
+
+    Directory.CreateDirectory(apisPath);
+    Directory.CreateDirectory(modelsPath);
+
+    foreach (var api in data)
+    {
+        var file = Path.Combine(apisPath, $"{api.TypeName}.cs");
+        File.WriteAllText(file, api.Code, Encoding.UTF8);
+        Console.WriteLine($"Interface output file：{file}");
+    }
+    foreach (var model in models)
+    {
+        var file = Path.Combine(modelsPath, $"{model.TypeName}.cs");
+        File.WriteAllText(file, model.Code, Encoding.UTF8);
+        Console.WriteLine($"Model output file：{file}");
+    }
+}, urlArg, nsOption);
+
+return await rootCommand.InvokeAsync(args);
